441c32a4983371783c9007c53247eba95a770f09
lavitzwind <leojimlavi@gmail.com>
2022-05-26 12:22:24 -0500

    project images

--- src/projects.js
+++ src/projects.js
@@ -5,8 +5,7 @@ export const projects = [
     technologies: ["React", "serverless functions"],
     description:
       "Cool Weather App is a React application that displays the current weather in a given city or country. It uses the OpenWeatherMap API to retrieve the weather data. This React App also features the geolocation API, persistent data storage and Serverless Functions.",
-    image:
-      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    image: "assets/mini-wea.png",
     demo: "http://cool-weather-app.vercel.app/",
     github: "https://github.com/lavitzwind/cool-weather-app",
   },
@@ -16,8 +15,7 @@ export const projects = [
     technologies: ["React", "Node", "Express", "MongoDB"],
     description:
       "Voyageur app is a travel app that allows users to pin locations and share your travel experiences with other users. It uses the mapbox API to retrieve the map data. Voyageur app includes a Node.js backend that uses MongoDB to store the user data.",
-    image:
-      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    image: "assets/mini-map.png",
     demo: "http://cool-travel-app-voyageur.vercel.app/",
     github: "https://github.com/lavitzwind/cool-travel-app-voyageur",
   },
@@ -27,8 +25,7 @@ export const projects = [
     technologies: ["React"],
     description:
       "Elden Ring is a React App that features information about the game Elden Ring. This app uses an Open Source API to retrieve the game data. The page features React Router to navigate between the different pages.",
-    image:
-      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    image: "assets/mini-elden.png",
     demo: "http://elden-ring-react.vercel.app/",
     github: "https://github.com/lavitzwind/elden-ring-react",
   },
@@ -38,8 +35,7 @@ export const projects = [
     technologies: ["React", "Node", "Express", "MongoDB", "NextJS"],
     description:
       "Ninja Maki is a food ordering app that allows users to order food from a list of available options and save their orders to a MongoDB database. Ninja Maki also features a Node.js backend that uses MongoDB to store the user data.",
-    image:
-      "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    image: "assets/mini-maki.png",
     demo: "https://ninja-maki-nextjs.vercel.app/",
     github: "https://github.com/lavitzwind/ninja-maki-nextjs",
   },
