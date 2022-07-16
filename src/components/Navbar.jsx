bb462965c9cf2a562581104b00a7bc2ce68cd9f4
lavitz <leojimlavi@gmail.com>
2022-07-16 04:22:09 -0500

    fixing links navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -191,16 +191,16 @@ const Navbar = () => {
         <ItemRight>
           <ul>
             <li>
-              <a href="/#">Home</a>
+              <a href="#">Home</a>
             </li>
             <li>
-              <a href="/#projects">Projects</a>
+              <a href="#projects">Projects</a>
             </li>
             <li>
-              <a href="/#about">About</a>
+              <a href="#about">About</a>
             </li>
             <li>
-              <a href="/#contact">Contact</a>
+              <a href="#contact">Contact</a>
             </li>
           </ul>
         </ItemRight>
