63f5ff930dbfe78d9b8f5ee678fd3d226129bda0
Leonardo D. Jimenez <45695003+lavitzwind@users.noreply.github.com>
2022-08-22 15:31:09 -0500

    Update projects.ts

--- src/projects.ts
+++ src/projects.ts
@@ -34,7 +34,7 @@ export const projects = [
     title: "Ninja Maki",
     technologies: ["React", "Node", "Express", "MongoDB", "NextJS"],
     description:
-      "Ninja Maki is a food ordering app that allows users to order food from a list of available options and save their orders to a MongoDB database. Ninja Maki also features a Node.js backend server.",
+      "Ninja Maki is a food ordering app that allows users to order food from a list of available options and save their orders to a MongoDB database. Ninja Maki also features a Next.js server-side rendering.",
     image: "assets/mini-maki.png",
     demo: "https://ninja-maki-nextjs.vercel.app/",
     github: "https://github.com/lavitzwind/ninja-maki-nextjs",
